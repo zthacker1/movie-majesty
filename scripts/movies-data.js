fa96e941fad2ca39a81f37c7c7159c31bbb61e74
Zachary Thacker <zthacker@outlook.com>
2024-05-30 12:51:05 -0500

    added database

---
+++ scripts/movies-data.js
@@ -0,0 +1,37 @@
+export const movies = [
+  {
+    title: "Inception",
+    poster:
+      "https://fr.web.img6.acsta.net/medias/nmedia/18/72/34/14/19453822.jpg",
+    description:
+      "A skilled thief is tasked with planting an idea in someone's subconscious mind.",
+  },
+  {
+    title: "The Matrix",
+    poster:
+      "https://image.tmdb.org/t/p/original/309gy3jIti5UDEPGiisNpRDv6Pa.jpg",
+    poster: "",
+    description:
+      "A computer hacker learns the true nature of his reality and his role in the war against the controllers of it.",
+  },
+  {
+    title: "Interstellar",
+    poster:
+      "https://assets.fontsinuse.com/static/use-media-items/21/20287/upto-700xauto/56703471/interstellar.jpeg?resolution=0",
+    description:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+  },
+  {
+    title: "Jurassic Park",
+    poster:
+      "https://i.pinimg.com/originals/58/2b/7e/582b7e5803b80101ada6114d176f5f16.jpg",
+    description:
+      "A pragmatic paleontologist visiting an almost complete theme park is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.",
+  },
+  {
+    title: "The Lion King",
+    poster: "https://www.movieartarena.com/imgs/lionkingintz.jpg",
+    description:
+      "Lion prince Simba and his father are targeted by his bitter uncle, who wants to ascend the throne himself.",
+  },
+];
